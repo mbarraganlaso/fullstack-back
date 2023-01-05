c01c93226f941ee5dfb766ce361151d2c2af98ab
Miguel Angel Barragan Laso <mbarraganlaso@gmail.com>
2023-01-05 18:54:26 +0100

    Cambios de checkeo statico pylint

--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
+"""
+API PizzaFullstack
+"""
 from flask import Flask, request, redirect
 import persistencia
 
@@ -5,6 +8,9 @@ app = Flask(__name__)
 
 @app.route("/pizza", methods=['POST'])
 def procesa_pedido():
+    """
+    Servicio para dar de alta un pedido
+    """
     nombre = request.form.get("nombre")
     apellidos = request.form.get("apellidos")
     persistencia.guardar_pedido(nombre, apellidos)
